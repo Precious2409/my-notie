bf8e99d9f8d096c3e28b2ee9f3293c06bce6bd71
Precious2409 <pinkypresh05@gmail.com>
2024-09-18 13:06:45 +0100

    Step 6

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -60,10 +60,13 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try{
-                final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
                     email: email,
                     password: password
                 );
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(verifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 if(e.code == 'weak-password'){
                   await showErrorDialog(context, 'Weak password');
--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:learningdart/constants/routes.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({super.key});
@@ -17,16 +18,26 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
       ),
       body: Column(
         children: [
-          const Text('Please verify your email address'),
+          const Text("We've sent you a code, please check your email."),
+          const Text("Please verify your email address if you haven't"),
           TextButton(
               onPressed: () async {
                 final user = FirebaseAuth.instance.currentUser;
                 await user?.sendEmailVerification();
               },
               child: const Text('Send email verification code')
+          ),
+          TextButton(
+              onPressed: () async {
+               await FirebaseAuth.instance.signOut();
+               Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
+              },
+              child: const Text('Restart')
           )
         ],
       ),
     );
   }
 }
+
+//I stopped at 14:35:40
